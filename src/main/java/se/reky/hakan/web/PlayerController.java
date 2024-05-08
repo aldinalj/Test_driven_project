545ee640d628ef4979ab6394dc6984c8858a07ab
aldinalj <aldinalj@gmail.com>
2024-05-08 16:42:55 +0200

    Added player endpoint

--- src/main/java/se/reky/hakan/web/PlayerController.java
+++ src/main/java/se/reky/hakan/web/PlayerController.java
@@ -3,6 +3,7 @@ package se.reky.hakan.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import se.reky.hakan.model.Player;
 import se.reky.hakan.service.PlayerService;
@@ -19,5 +20,11 @@ public class PlayerController {
     public String listPlayers(Model model) {
         model.addAttribute("players", playerService.findAllPlayersSortedByExperience());
         return "players"; // Name of the Thymeleaf template
+    }
+
+    @GetMapping("/player/{id}")
+    public String singlePlayer(@PathVariable long id, Model model) {
+        model.addAttribute("player", playerService.findPlayerById(id));
+        return "player";
     }
 }
